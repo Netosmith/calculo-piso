82bbd656566aa3f8ae8371da3833c8661fc8585b
Netosmith <luziano.transportes@gmail.com>
2026-03-10 20:24:48 -0300

    Update home-solicitacoes.js

--- assets/js/home-solicitacoes.js
+++ assets/js/home-solicitacoes.js
@@ -2,7 +2,7 @@
   "use strict";
 
   const API_URL =
-    "https://script.google.com/macros/s/AKfycbyWSvivxCp4GslWhGRxfvg6gcE72hNATplIqdVG2tp46DtrdxWzKLqirm-BgcSv2tlw/exec";
+    "https://script.google.com/macros/s/AKfycby95ZPwK5pwuU8hsydVY3Flr9tl8esHKMl0b9IlpDTP7FQEPOwrvxWZJj0uZOlANh_Z/exec";
 
   const FILIAIS = [
     "ITUMBIARA","RIO VERDE","JATAI","MINEIROS","CHAPADAO DO CEU","MONTIVIDIU",

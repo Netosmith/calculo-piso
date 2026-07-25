52d0539dda63ba1b2943b7df485bc85d3783fd4b
Netosmith <luziano.transportes@gmail.com>
2026-07-25 10:41:07 -0300

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,5 +1,5 @@
 // =====================================================
-// auth.js | NOVA FROTA
+// auth.js | NOVA FROTA-
 // Login via Google Apps Script + seleção de estado
 // Senhas removidas do navegador
 // =====================================================

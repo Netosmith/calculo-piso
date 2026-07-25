6552c085461d2a93c000cc1268377b821a0ef5b1
Netosmith <luziano.transportes@gmail.com>
2026-07-25 10:12:07 -0300

    Update bi.js

--- assets/js/bi.js
+++ assets/js/bi.js
@@ -3,7 +3,7 @@
   "use strict";
 
   const API_URL =
-    "https://script.google.com/macros/s/AKfycbxqYIjETvyUF3DF_ovqQQ5NORRePQw2dxH2dgrERigGsEitSLH1qbLwO3kVrpTYoUfG/exec";
+    "https://script.google.com/macros/s/AKfycbxcxoPf9iOShrihdONx721Sd327kszq044hhGO8JDHljierx4TauTLugwfXA27XvRri/exec";
 
   const PESO_MEDIO = 38;
   const AUTO_REFRESH_MS = 60000;

351adaef74e5fbdbead2cc34b6f205a631b129cb
Netosmith <luziano.transportes@gmail.com>
2026-07-28 09:04:39 -0300

    Update calculo-piso.js

--- assets/js/calculo-piso.js
+++ assets/js/calculo-piso.js
@@ -2,7 +2,7 @@
 (function(){
 "use strict";
 
-const API_URL="https://script.google.com/macros/s/AKfycbxcxoPf9iOShrihdONx721Sd327kszq044hhGO8JDHljierx4TauTLugwfXA27XvRri/exec";
+const API_URL="https://script.google.com/macros/s/AKfycbwlz0Rr0PmdPLZva-6TtSzpfDqx-G1IAkrX8n8cFp5t4mDkH5NQjsztvaWYbtUu8nFG/exec";
 const ADMIN_USER="LUZIANO";
 const QUICK_QUOTE_KEY=`piso_${userSafeKey_()}_cotacao_rapida_v1`;
 
